ccdd3053cde5afd00e3700d853c2eada009ade04
Aarón Sacristán <aaronsacristan6a@gmail.com>
2025-05-14 09:55:58 +0200

    Cambios5

---
+++ scheduler.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple
+from proceso import Proceso
+
+GanttEntry = Tuple[str, int, int]
+
+class Scheduler(ABC):
+    @abstractmethod
+    def planificar(self, procesos: List[Proceso]) -> List[GanttEntry]:
+        pass
